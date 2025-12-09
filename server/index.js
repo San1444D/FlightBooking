973439e81017b8c678d1b2fbbaf1fdfb7e7afe91
San1444D <146318663+San1444D@users.noreply.github.com>
2025-12-09 21:24:42 +0530

    trust proxy - 1

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ try {
 }
 
 const app = express()
-app.set('trust proxy', 'uniquelocal');
+app.set('trust proxy', 1);
 // --- Middleware ---
 // Parse JSON bodies (use express.json() directly in newer projects if preferred)
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
