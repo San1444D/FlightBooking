b8ad268f0f6a6497507bcaab916f248f2c70e797
San1444D <146318663+San1444D@users.noreply.github.com>
2025-12-11 08:51:14 +0530

    update .\index.html logo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/FlightBooking">
       <Provider store={store}>
         <App />
         <ToastContainer
